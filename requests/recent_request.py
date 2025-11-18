ab1b895a41aeece1afd25d400eac21a6badd7f07
Ruel <elurl115@gmail.com>
2025-11-18 15:27:38 +0800

    refactor: recent selector

--- requests/recent_request.py
+++ requests/recent_request.py
@@ -1,11 +1,5 @@
 from pydantic import BaseModel, HttpUrl
 
 class RecentRequest(BaseModel):
-    url: HttpUrl
+    source: str
     page: int
-    item_selector: str
-    rating_selector: str
-    title_selector: str
-    thumbnail_selector: str
-    chapter_selector: str
-    chapter_link_selector: str
--- services/recent_service.py
+++ services/recent_service.py
@@ -2,27 +2,30 @@ import httpx
 from bs4 import BeautifulSoup
 from requests.recent_request import RecentRequest
 from helpers.global_helper import extract_title, extract_thumbnail, extract_chapters
+from helpers.selector_helper import get_selector
 
 class RecentService:
     async def get_recent_items(self, request: RecentRequest):
-        url = str(request.url)
+        selector = get_selector(request.source)
+        selectors = selector._selector("recent.json")
+        url = selectors["based_url"]
 
         if(request.page > 1):
-            url += 'page/' + str(request.page) + '/'
+            url += f"/page/{request.page}/"
 
         async with httpx.AsyncClient(timeout=15.0) as client:
             response = await client.get(url, headers={"User-Agent": "MyScraperBot/1.0"})
             response.raise_for_status()
 
         soup = BeautifulSoup(response.text, "lxml")
-        recent_items = soup.select(request.item_selector)
+        recent_items = soup.select(selectors["item_selector"])
         results = []
 
         for item in recent_items:
-            ratings = self.extract_ratings(item, request.rating_selector)
-            title = extract_title(item, request.title_selector)
-            thumbnail = extract_thumbnail(item, request.thumbnail_selector)
-            chapters = extract_chapters(item, request.chapter_selector, request.chapter_link_selector)
+            ratings = self.extract_ratings(item, selectors["rating_selector"])
+            title = extract_title(item, selectors["title_selector"])
+            thumbnail = extract_thumbnail(item, selectors["thumbnail_selector"])
+            chapters = extract_chapters(item, selectors["chapter_selector"], selectors["chapter_link_selector"])
 
             results.append({
                 "title": title.get("text"),
